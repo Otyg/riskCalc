9a15ad60aba3c0f2c5c65cff3d51245597400735
Martin Vesterlund <Otyg@users.noreply.github.com>
2026-01-12 01:24:26 +0100

    Change calculations (#7)
    * Change TEF calculation
    
    * Inga beräkningar görs i app.py, beräkningar görs av questionaire
    * TEF representeras som händelser/år för alla alternativ i default formuläret
    * TEF är medelvärdet av avgivna svar istället för bas*modifikationer
    
    * Fix loading of categories.json
    
    * Change MonteCarlo
    
    * Fixade bugg i questionaire
    * PertDistribution istället för triangeldistribution i MonteCarlo
    
    * Uppdaterat TEF för privacy.json

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def create_scenario_page(request: Request, draft_id: str, qset: str = DEFAULT_QU
     category_suggestions = categories_repo.load()
 
     try:
-        qs = questionaires_repo.load_objects(qset)  # {"tef": Questionaire, "vuln": ..., "lm": ...}
+        qs = questionaires_repo.load_objects(qset)
     except FileNotFoundError:
         # om set saknas: visa tomt + fel
         qs = {"tef": None, "vuln": None, "lm": None}
@@ -269,13 +269,23 @@ async def create_scenario_save(request: Request, draft_id: str):
         )
 
     try:
+        print(qs.get('tef').mean())
         questionaires = Questionaires(tef=qs.get('tef'), vuln=qs.get('vuln'), lm=qs.get('lm'))
-        tef = qs.get('tef').multiply_factor()
-        vuln_score = qs.get('vuln').sum_factor()
-        loss_magnitude = qs.get('lm').range()
-        risk = DiscreteRisk(tef=tef, vuln_score=vuln_score, loss_magnitude=loss_magnitude, budget=Decimal(risk_dict.get('budget')), currency=risk_dict.get('currency'))
-        scenario_obj = RiskScenario(name=name, category=category ,actor=actor, asset=asset, threat=threat, vulnerability=vulnerability, description=description, risk=risk, questionaires=questionaires)
-        scenario_json = scenario_obj.to_dict()
+        questionaires_values = questionaires.calculate_questionairy_values()
+        risk = DiscreteRisk(tef=questionaires_values.get('tef'),
+                            vuln_score=questionaires_values.get('vuln'),
+                            loss_magnitude=questionaires_values.get('lm'),
+                            budget=Decimal(risk_dict.get('budget')),
+                            currency=risk_dict.get('currency'))
+        scenario_obj = RiskScenario(name=name,
+                                    category=category,
+                                    actor=actor,
+                                    asset=asset,
+                                    threat=threat,
+                                    vulnerability=vulnerability,
+                                    description=description,
+                                    risk=risk,
+                                    questionaires=questionaires)
     except Exception as e:
         raise e
 
@@ -373,12 +383,21 @@ async def edit_scenario_save(request: Request, draft_id: str, scenario_index: in
 
     try:
         questionaires = Questionaires(tef=qs.get('tef'), vuln=qs.get('vuln'), lm=qs.get('lm'))
-        tef = qs.get('tef').multiply_factor()
-        vuln_score = qs.get('vuln').sum_factor()
-        loss_magnitude = qs.get('lm').range()
-        risk = DiscreteRisk(tef=tef, vuln_score=vuln_score, loss_magnitude=loss_magnitude, budget=Decimal(risk_dict.get('budget')), currency=risk_dict.get('currency'))
-        scenario_obj = RiskScenario(name=name, category=category ,actor=actor, asset=asset, threat=threat, vulnerability=vulnerability, description=description, risk=risk, questionaires=questionaires)
-        scenario_json = scenario_obj.to_dict()
+        questionaires_values = questionaires.calculate_questionairy_values()
+        risk = DiscreteRisk(tef=questionaires_values.get('tef'),
+                            vuln_score=questionaires_values.get('vuln'),
+                            loss_magnitude=questionaires_values.get('lm'),
+                            budget=Decimal(risk_dict.get('budget')),
+                            currency=risk_dict.get('currency'))
+        scenario_obj = RiskScenario(name=name,
+                                    category=category,
+                                    actor=actor,
+                                    asset=asset,
+                                    threat=threat,
+                                    vulnerability=vulnerability,
+                                    description=description,
+                                    risk=risk,
+                                    questionaires=questionaires)
     except Exception as e:
         raise e
 
